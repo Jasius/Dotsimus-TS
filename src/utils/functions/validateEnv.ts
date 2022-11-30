c7079af01baa7abe997a0c37920a6cc8dab5f02b
Koji Odyssey <cldoc507@gmail.com>
2022-11-30 18:40:01 -0500

    Validate DATABASE_URL

--- src/utils/functions/validateEnv.ts
+++ src/utils/functions/validateEnv.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
     const ProcessEnv = z.object({
         NODE_ENV: z.string().regex(/^(production|development)$/),
         DISCORD_TOKEN: z.string(),
+        DATABASE_URL: z.string().url(),
         TOPGG_TOKEN: z.string().optional()
     });
 
